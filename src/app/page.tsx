446dfd888b3d6b0f9a5978793d11ee94c3a3723b
Kristine Fernandez <hello@kristinefernandez.com>
2025-10-26 10:56:35 -0500

    question revisions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="text-center lg:text-2xl">
+    <div className="text-center md:text-xl lg:text-2xl">
       <p className="mb-3 font-bold">This experience requires introspection and vulnerability.<br /> If you are willing to take this on, proceed.</p>
       <Link
         href="/questionnaire"
         className="inline-block underline hover:no-underline"
       >
-        I'm Ready
+        I am ready &#8594;
       </Link>
     </div>
   );
